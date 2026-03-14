5b8ffd14e399ee586823a55d27e7d9500afa584f
korjje617 <korjje617@gmail.com>
2026-03-15 05:37:09 +0900

    단기예보 구현 최종의 최종

--- backend/ASG/src/main/java/com/example/demo/APItest/GoogleReviewRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.APItest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GoogleReviewRepository extends JpaRepository<GoogleReviewEntity, Long> {
-}
